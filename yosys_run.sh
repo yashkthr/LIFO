aea9791a4d7cb0285ecf450a63a4b317afe5564e
Yash Kothari <69398841+yashkthr@users.noreply.github.com>
2022-07-28 10:52:54 +0530

    Add files via upload

---
+++ yosys_run.sh
@@ -0,0 +1,14 @@
+# read design
+
+read_verilog iiitb_lifo.v
+
+# generic synthesis
+synth -top iiitb_lifo
+
+# mapping to mycells.lib
+dfflibmap -liberty /home/yashkthr/Desktop/iiitb_lifo/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/yashkthr/Desktop/iiitb_lifo/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+clean
+flatten
+# write synthesized design
+write_verilog -noattr synth_ripple_carry_adder.v
